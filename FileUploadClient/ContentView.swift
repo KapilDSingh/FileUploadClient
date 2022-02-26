deb463a7bbc796924f1bc976eb3dae210d0b0816
KapilDSingh <KapilSingh@msn.com>
2022-02-26 08:00:23 -0500

    Initial Commit

--- FileUploadClient/ContentView.swift
+++ FileUploadClient/ContentView.swift
@@ -1,21 +1,51 @@
 //
 //  ContentView.swift
-//  FileUploadClient
+//  SwiftUploadFile
 //
-//  Created by Kapil Singh on 2/26/22.
+//  Created by Kapil Singh on 2/13/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        VStack {
+            
+            Button("Upload Files") {
+                
+                print("Application directory: \(NSHomeDirectory())")
+                        
+                
+                // Start an async task
+                Task {
+                    
+                    do {
+                        guard let saveFolderURL = URL.createFolder(folderName: dateTimeString()) else {return}
+                        _ = URL.createFile(folderURL: saveFolderURL, fileName: "scanData.txt")
+                        _ = URL.createFile(folderURL: saveFolderURL, fileName: "scanData2.txt")
+                        print ("saveFolder = ", saveFolderURL)
+                        let FileFetcher = FilesUploader()
+                        try await FileFetcher.uploadFileWithAsyncURLSession(folderURL:saveFolderURL)
+                        
+                        
+                    } catch {
+                        print("Request failed with error: \(error)")
+                    }
+                    
+  /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
 
+        }
+        }
+    }
+       
+        
+        
+}
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
