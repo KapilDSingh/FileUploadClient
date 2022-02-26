12007c5a3ab852bcbae91e057622230d083ae1d7
KapilDSingh <KapilSingh@msn.com>
2022-02-26 06:54:53 -0500

    Initial Commit

---
+++ FileUploadClient/FileUploadClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileUploadClientApp.swift
+//  FileUploadClient
+//
+//  Created by Kapil Singh on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FileUploadClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
